0efce32066df058cb12bfb3c7565422c2d82aa75
keita arimitsu <39061364+keitaarimitsu@users.noreply.github.com>
2021-03-15 15:30:56 +0100

    Update hamiltonians.py

--- hamiltonians.py
+++ hamiltonians.py
@@ -14,8 +14,46 @@ def pxp_1d(n_site: int, state_vecs):
   """
   pxp = np.zeros((len(state_vecs), len(state_vecs)))
   
-  for i in range(len(state_vecs)):
-      tmp_str = state_vecs[i]
-      for k in range(n_site):
-          if k == 0:
-  return pxp
+  if n_site % 2 == 1:
+      raise TypeError("the number of particles must be even.")
+    
+  else:
+      for i in range(len(state_vecs)):
+          tmp_str = state_vecs[i]
+          for k in range(n_site):
+              if k == 0:
+                  if (tmp_str[k + 1] == "1") and (tmp_str[int(n_site / 2 - 1)] == "1"):
+                      if tmp_str[k] == "0":
+                          aft_str = tmp_str[k + 1:]
+                          act_str = "1" + aft_str
+                      else:
+                          aft_str = tmp_str[k + 1:]
+                          act_str = "0" + aft_str
+                          
+                      idx = state_vecs.index(act_str)
+                      pxp[idx][i] += 1
+              elif k == int(n_site / 2 -1):
+                  if (tmp_str[k - 1] == "1") and (tmp_str[0] == "1"):
+                      if tmp_str[k] == "0":
+                          bef_str = tmp_str[0:k]
+                          act_str = bef_str + "1"
+                      else:
+                          bef_str = tmp_str[0:k]
+                          act_str = bef_str + "0"
+                          
+                      idx = state_vecs.index(act_str)
+                      pxp[idx][i] += 1
+              else:
+                  if (tmp_str[k - 1] == "1") and (tmp_str[k + 1] == "1"):
+                      if tmp_str[k] == "0":
+                          bef_str = tmp_str[0:k]
+                          aft_str = tmp_str[k + 1:]
+                          act_str = bef_str + "1" + aft_str
+                      else:
+                          bef_str = tmp_str[0:k]
+                          aft_str = tmp_str[k + 1:]
+                          act_str = bef_str + "0" + aft_str
+                              
+                      idx = state_vec.index(act_str)
+                      pxp[idx][i] += 1
+      return pxp  
