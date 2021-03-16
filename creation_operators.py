6e9c755c7cb3470051e21f8205559bbc9ce76033
keita arimitsu <39061364+keitaarimitsu@users.noreply.github.com>
2021-03-16 16:50:17 +0100

    Update creation_operators.py

--- creation_operators.py
+++ creation_operators.py
@@ -34,7 +34,25 @@ def constraint_lowering1d(
             operator[idx][i] += 1
     return csc_matrix(operator)
         
-    
+
+def one_body_island_creation1d(
+    n_site: int,
+    state_vecs: "np.array",
+    pos: int
+) -> "scipy.sparse.csc_matrix":
+    operator = np.zeros((len(state_vecs), len(state_vecs)), dtype="int32")
+    for i in range(len(state_vecs)):
+        tmp_str = state_vecs[i]
+        cond_str = "".join([tmp_str[(pos + k) % n_site] for k in range(-1, 2, 1)])
+        if cond_str == "101":
+            tmp_str_list  = list(tmp_str)
+            tmp_str_list[pos] = "1"
+            act_str = "".join(tmp_str_list)
+            idx = state_vecs.index(act_str)
+            operator[idx][i] += 1
+    return operator
+
+
 def one_body_bound_state_creation1d(
     n_site: int,
     state_vecs: "np.array",
@@ -55,10 +73,10 @@ def one_body_bound_state_creation1d(
     operator = np.zeros((len(state_vecs), len(state_vecs)), dtype="int32")
     for i in range(len(state_vecs)):
         tmp_str = state_vecs[i]
-        cond_str = "".join([tmp_str[(b + k) % n_site] for k in range(-2, 3, 1)]) 
+        cond_str = "".join([tmp_str[(pos + k) % n_site] for k in range(-2, 3, 1)]) 
         if cond_str == "11111":
             tmp_str_list = list(tmp_str)
-            tmp_str_list[b] = "0"
+            tmp_str_list[pos] = "0"
             act_str = "".join(tmp_str_list)
             idx = state_vecs.index(act_str)
             operator[idx][i] += 1
