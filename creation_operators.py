37ae25220a9e8d49fec1441ffb748f0690cbae5b
keita arimitsu <39061364+keitaarimitsu@users.noreply.github.com>
2021-03-17 17:15:59 +0100

    Update creation_operators.py

--- creation_operators.py
+++ creation_operators.py
@@ -35,6 +35,42 @@ def constraint_lowering1d(
     return csc_matrix(operator)
         
 
+def constraint_bound_creation1d(
+    n_site: int,
+    state_vecs: "np.array",
+    pos1: int,
+    pos2: int
+) -> "scipy.sparse.csc_matrix":
+     """construct a pair of an island @ pos1 and the bound state @ pos2, i.e., a_[pos1]^dag γ_[pos2]^dag
+    Args: 
+        n_site (int):
+            the number of spins
+        state_vecs (np.array):
+            1d array which represents states under consideration, 
+            i.e., state_vecs[i] = "10101110101011..." (ith configuration)
+        pos1 (int):
+            position of an island
+        pos2 (int):
+            position of a bound state
+    Return:
+        operator (scipy.sparse.csc_matrix): a matrix corresponding a_[pos1]^dag γ_[pos2]^dag
+    """
+    operator = np.zeros((len(state_vecs), len(state_vecs)), dtype="int32")
+    for i in range(len(state_vecs)):
+        tmp_str = state_vecs[i]
+        cond_str_pos1 = "".join([tmp_str[(pos1 + k) % n_site] for k in range(-1, 2, 1)])
+        cond_str_pos2 = "".join([tmp_str[(pos2 + k) % n_site] for k in range(-2, 3, 1)])
+        cond_str = cond_str_pos1 + cond_str_pos2
+        if cond_str == "10111111":
+            tmp_str_list = list(tmp_str)
+            tmp_str_list[pos1] = "1"
+            tmp_str_list[pos2] = "0"
+            act_str = "".join(tmp_str_list)
+            idx = state_vecs.index(act_str)
+            operator[idx][i] += 1
+    return operator
+
+
 def one_body_island_creation1d(
     n_site: int,
     state_vecs: "np.array",
